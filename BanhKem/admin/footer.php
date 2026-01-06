4aa8bb78fc638c7228ff085528b0fa1309d41457
PhamThiNhuY <152623220+PhamThiNhuY@users.noreply.github.com>
2026-01-06 11:57:04 +0700

    Delete BanhKem/admin directory

--- BanhKem/admin/footer.php
+++
@@ -1,6 +0,0 @@
-<footer>
-    <!-- Bản quyền -->
-    <div class="footer-bottom text-center">
-        ©2025 Bản quyền thuộc về <span>nhuybakery.com</span>
-    </div>
-</footer>
--- BanhKem/admin/sanpham_xoa.php
+++
@@ -1,7 +0,0 @@
-<?php
-include '../config/connect.php';
-$id = $_GET['id'];
-
-mysqli_query($mysqli, "DELETE FROM san_pham WHERE MaBanh=$id");
-header("Location: sanpham.php");
-?>
